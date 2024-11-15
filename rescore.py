c8bb2e75bb8e8e2965014771803a11fb11dbd9a5
wangmengzhi <admin@mengzhi.wang>
2024-11-15 16:23:01 +0800

    Update rescore.py

--- rescore.py
+++ rescore.py
@@ -22,21 +22,12 @@ total_sub=0
 total_ins=0
 total_dele=0
 cnt=0
-recs=[]
-scores=[]
+max_score=-float('inf')
 start_time = timeit.default_timer()
 with open('nbest.txt', encoding='utf-8') as f:
     for l in f:
         l=l.strip().split(',')
         if len(l)==1:
-            max_score=-float('inf')
-            for rec, score in zip(recs, scores):
-                r=' '.join(jieba.cut(rec.replace(' ',''), HMM=False))
-                lm_score=model.score(r, bos = False, eos = False)
-                score=score+lm_score*lm_weight
-                if score>max_score:
-                    max_score=score
-                    best=rec
             (d,ins,dele,sub),l = wer(l[0], best)
             total_d+=d
             total_l+=l            
@@ -45,12 +36,16 @@ with open('nbest.txt', encoding='utf-8') as f:
             total_dele+=dele
             cnt+=1
             if cnt%1000==0:print(cnt)
-            recs.clear()
-            scores.clear()
+            max_score=-float('inf')
         else:
             rec,score=l
-            recs.append(rec)
-            scores.append(float(score))
+            r=' '.join(jieba.cut(rec.replace(' ',''), HMM=False))
+            lm_score=model.score(r, bos = False, eos = False)
+            score=float(score)+lm_score*lm_weight
+            if score>max_score:
+                max_score=score
+                best=rec
+
 print('wer:%.2f S=%.2f I=%.2f D=%.2f'%(total_d/total_l*100,total_sub/total_l*100,total_ins/total_l*100,total_dele/total_l*100))
 time=timeit.default_timer()-start_time
 print('time:',int(time%3600//60),'m',int(time%60),'s')
